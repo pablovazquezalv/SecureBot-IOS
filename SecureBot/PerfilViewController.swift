c687a9d3a762b87a6c1a5e59f17a84b7d3c8de72
Pablo <pabloalvaradovazquez10@gmail.com>
2023-04-14 09:57:03 -0600

    h

--- SecureBot/PerfilViewController.swift
+++ SecureBot/PerfilViewController.swift
@@ -16,14 +16,12 @@ class PerfilViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+ 
+   
+    @IBAction func jjjj(_ sender: Any)
+    {
+        print("hola")
     }
-    */
+    
 
 }
