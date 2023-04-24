1a74620c7e287bb3c11269b4f19095611d9a8781
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-04-23 18:00:10 -0600

    Consulta de sensores desde el inicio

--- SecureBot/Controllers/SensoresViewController.swift
+++ SecureBot/Controllers/SensoresViewController.swift
@@ -7,66 +7,55 @@
 
 import UIKit
 class SensoresViewController: UIViewController {
-
-    
-    var sensor:[Sensor] = []
-    let userData = UserData.sharedData()
     @IBOutlet weak var srcSensores: UIScrollView!
-    var nombre:[String] = []
-    var codigos:[String] = []
+    var sensor: [Sensor] = []
+    let userData = UserData.sharedData()
+    var nombre: [String] = []
+    var codigos: [String] = []
     var contador = 0
-    
-
-    
-    override func viewDidLoad()
-    {
+        
+    override func viewDidLoad() {
         super.viewDidLoad()
+        consultarServicio()
+        
         Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { timer in
             self.consultarServicio()
-           }
+        }
     }
     
-    func consultarServicio()
-        {
-            let conexion = URLSession(configuration: .default)
-            let url = URL(string: "https://securebot.ninja/api/v1/ultimoDato")!
-            let token = userData.token
-            var urlRequest = URLRequest(url: url)
-            urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(token)"]
+    func consultarServicio() {
+        let conexion = URLSession(configuration: .default)
+        let url = URL(string: "https://securebot.ninja/api/v1/ultimoDato")!
+        let token = userData.token
+        var urlRequest = URLRequest(url: url)
+        urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(token)"]
             
-            conexion.dataTask(with: urlRequest) {datos, respuesta, error in
-                
-                do{
-                    let json = try JSONSerialization.jsonObject(with: datos!,options: []) as? [Any]
+        conexion.dataTask(with: urlRequest) { datos, respuesta, error in 
+                do {
+                    let json = try JSONSerialization.jsonObject(with: datos!, options: []) as? [Any]
                     if let resultados = json {
                         self.sensor.removeAll()
-                        for carro in  resultados
-                        {
-                            if let datos = carro as? [String:Any]
-                            {
-                        
-                                self.sensor.append(Sensor(nombre:  datos["tipo"] as! String, valor: datos["valores"] as! String))                            }
+                        for carro in resultados {
+                            if let datos = carro as? [String: Any] {
+                                self.sensor.append(Sensor(nombre: datos["tipo"] as! String, valor: datos["valores"] as! String))                            
+                            }
                         }
+                        
                         DispatchQueue.main.async {
-                            self.dibujarEpisodios()
-                        }
-                        
+                            self.dibujarSensores()
+                        } 
                     }
-                }catch
-                {
+                } catch {
                     print("errores")
                 }
             }.resume()
         }
      
-        func dibujarEpisodios()
-    {
+        func dibujarSensores() {
         var y = 10
-        for i in 0..<sensor.count
-        {
+        for i in 0..< sensor.count {
             let vista = UIView(frame: CGRect(x: 10, y: y, width: Int(srcSensores.frame.width) - 20, height: 70))
             vista.backgroundColor = .white
-            
             
             let nombre = UILabel(frame: CGRect(x: 5, y: 5, width: vista.frame.width - 10, height: 25))
             nombre.text = sensor[i].nombre
@@ -89,16 +78,13 @@ class SensoresViewController: UIViewController {
             spinner.progress = progress
                         
             spinner.center = CGPoint(x: vista.frame.width - spinner.frame.width/2 - 5, y: vista.frame.height/2)
-                        vista.addSubview(spinner)
-           
-            
+            vista.addSubview(spinner)
             vista.addSubview(nombre)
             vista.addSubview(fecha)
             srcSensores.addSubview(vista)
             y += 80
         }
-        
+      
         srcSensores.contentSize = CGSize(width: 0, height: y)
-    }
-
+     }
 }
